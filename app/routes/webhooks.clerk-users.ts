567d64f5f6a50cb421016bad90d38218eb05a057
Josh Parrett <jtparrett@me.com>
2024-02-18 09:36:46 +0000

    webhook logging

--- app/routes/webhooks.clerk-users.ts
+++ app/routes/webhooks.clerk-users.ts
@@ -1,9 +1,17 @@
-import type { ActionFunctionArgs } from "@remix-run/node";
+import { json, type ActionFunctionArgs } from "@remix-run/node";
 import { prisma } from "~/utils/prisma.server";
 import { z } from "zod";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
+  if (request.method !== "POST") {
+    return json({ message: "Method not allowed" }, 405);
+  }
+
   const { type, data } = await request.json();
+  console.log("New User", {
+    data,
+    type,
+  });
 
   const userData = z
     .object({
@@ -24,5 +32,5 @@ export const action = async ({ request }: ActionFunctionArgs) => {
       throw new Error("Unsupported clerk event");
   }
 
-  return {};
+  return json({ success: true }, 200);
 };
