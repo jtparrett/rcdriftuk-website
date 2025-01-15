01b6d6e386ff4958d376a4aee4c15205c6eb2e95
Josh Parrett <jtparrett@me.com>
2025-01-15 22:33:43 +0000

    fix early access code

--- app/routes/events.$id.ticket._index.tsx
+++ app/routes/events.$id.ticket._index.tsx
@@ -9,8 +9,8 @@ import { stripe } from "~/utils/stripe.server";
 export const loader = async (args: LoaderFunctionArgs) => {
   const { params } = args;
   const { userId } = await getAuth(args);
-  const searchParams = new URLSearchParams(args.request.url);
-  const earlyAccessCode = searchParams.get("code");
+  const url = new URL(args.request.url);
+  const earlyAccessCode = url.searchParams.get("code");
 
   invariant(userId, "Unauthorized");
 
