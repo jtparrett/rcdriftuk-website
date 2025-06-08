3f9b23331db651d778cbfefdf74e5f76deb56c8e
Josh Parrett <jtparrett@me.com>
2025-06-08 13:53:37 +0100

    remove times from listing

--- app/components/EventTicketStatus.tsx
+++ app/components/EventTicketStatus.tsx
@@ -2,7 +2,6 @@ import type { GetEvent } from "~/utils/getEvent.server";
 
 import { styled } from "~/styled-system/jsx";
 import { format, isBefore } from "date-fns";
-import { getTime } from "~/utils/getEventDate";
 import { toZonedTime } from "date-fns-tz";
 interface Props {
   event: Pick<NonNullable<GetEvent>, "enableTicketing" | "ticketReleaseDate">;
@@ -35,8 +34,7 @@ export const EventTicketStatus = ({ event, isSoldOut }: Props) => {
     if (isBeforeRelease) {
       return (
         <styled.p color="brand.500" fontWeight="semibold" fontSize="sm">
-          Tickets Release {format(ticketReleaseDate, "do MMM, yyyy")}{" "}
-          {getTime(ticketReleaseDate)}
+          Tickets Release {format(ticketReleaseDate, "MMMM do")}
         </styled.p>
       );
     }
