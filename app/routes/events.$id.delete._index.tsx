9d2ea095ac8de10fb1c7efb3024338f4df259805
Josh Parrett <jtparrett@me.com>
2025-06-14 07:40:25 +0100

    delete recurring events

--- app/routes/events.$id.delete._index.tsx
+++ app/routes/events.$id.delete._index.tsx
@@ -3,6 +3,7 @@ import { redirect } from "react-router";
 import { Form, useLoaderData } from "react-router";
 import { z } from "zod";
 import { Button, LinkButton } from "~/components/Button";
+import { Select } from "~/components/Select";
 import { Box, Container, Flex, styled } from "~/styled-system/jsx";
 import { getAuth } from "~/utils/getAuth.server";
 import { prisma } from "~/utils/prisma.server";
@@ -47,6 +48,10 @@ export const action = async (args: ActionFunctionArgs) => {
   const { params } = args;
   const { id } = z.object({ id: z.string() }).parse(params);
   const { userId } = await getAuth(args);
+  const formData = await args.request.formData();
+  const deleteType = z
+    .enum(["single", "recurring"])
+    .parse(formData.get("deleteType"));
 
   if (!userId) {
     throw new Response(null, {
@@ -55,7 +60,7 @@ export const action = async (args: ActionFunctionArgs) => {
     });
   }
 
-  await prisma.events.delete({
+  const event = await prisma.events.findFirst({
     where: {
       id,
       eventTrack: {
@@ -66,7 +71,56 @@ export const action = async (args: ActionFunctionArgs) => {
         },
       },
     },
+    include: {
+      eventTrack: true,
+    },
   });
+
+  if (!event) {
+    throw new Response(null, {
+      status: 404,
+      statusText: "Not Found",
+    });
+  }
+
+  if (deleteType === "single") {
+    await prisma.eventResponses.deleteMany({
+      where: {
+        eventId: id,
+      },
+    });
+
+    await prisma.events.delete({
+      where: {
+        id,
+      },
+    });
+  } else if (deleteType === "recurring") {
+    await prisma.eventResponses.deleteMany({
+      where: {
+        event: {
+          createdAt: {
+            equals: event.createdAt,
+          },
+          trackId: event.trackId,
+        },
+      },
+    });
+
+    // Delete all recurring instances of this event
+    await prisma.events.deleteMany({
+      where: {
+        trackId: event.trackId,
+        createdAt: {
+          equals: event.createdAt,
+        },
+      },
+    });
+  }
+
+  if (event.eventTrack) {
+    return redirect(`/tracks/${event.eventTrack.slug}`);
+  }
 
   return redirect("/");
 };
@@ -76,26 +130,22 @@ export const EventDeletePage = () => {
 
   return (
     <Container maxW={1100} px={2} py={10}>
-      <Box
-        bg="gray.900"
-        p={8}
-        borderRadius="2xl"
-        mx="auto"
-        maxW={500}
-        textAlign="center"
-      >
+      <Box bg="gray.900" p={8} borderRadius="2xl" mx="auto" maxW={500}>
         <styled.h1 fontWeight="extrabold" fontSize="2xl">
           Delete "{event.name}"
         </styled.h1>
         <Form method="delete">
-          <styled.p mb={4}>
-            Are you sure you want to delete this event?
-          </styled.p>
-          <Flex gap={2} justifyContent="center">
+          <Select my={4} name="deleteType">
+            <option value="single">Delete only this event</option>
+            <option value="recurring">
+              Delete this event and all recurring instances of this event
+            </option>
+          </Select>
+          <Flex gap={2} justifyContent="flex-end">
             <LinkButton to={`/events/${event.id}`} variant="secondary">
               Cancel
             </LinkButton>
-            <Button type="submit">Yes, Delete this event</Button>
+            <Button type="submit">Delete Event</Button>
           </Flex>
         </Form>
       </Box>
