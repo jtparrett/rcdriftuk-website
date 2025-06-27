9955048b12bd95dea2e77be165acbbd98d66f381
Josh Parrett <jtparrett@me.com>
2025-06-27 11:16:17 +0100

    disable scaling

--- app/root.tsx
+++ app/root.tsx
@@ -65,7 +65,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
         <Meta />
         <Links />
         {process.env.NODE_ENV === "production" && (
