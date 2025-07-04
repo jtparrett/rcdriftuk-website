51adc8408f1f4c837e3029f4b52710a2b0b37506
Josh Parrett <jtparrett@me.com>
2025-07-04 15:55:23 +0100

    add meta content fit-cover tag

--- app/root.tsx
+++ app/root.tsx
@@ -67,7 +67,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"
         />
         <Meta />
         <Links />
