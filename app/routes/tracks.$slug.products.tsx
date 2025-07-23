44e07c8c306e8c81bf2ee2eadeca042936b24dad
Josh Parrett <jtparrett@me.com>
2025-07-23 10:05:31 +0100

    product page pagination

--- app/routes/tracks.$slug.products.tsx
+++ app/routes/tracks.$slug.products.tsx
@@ -1,12 +1,16 @@
+import { RiArrowLeftSLine, RiArrowRightSLine } from "react-icons/ri";
 import { useLoaderData, type LoaderFunctionArgs } from "react-router";
 import { z } from "zod";
+import { LinkButton } from "~/components/Button";
 import { ProductCard } from "~/components/ProductCard";
-import { Grid, styled } from "~/styled-system/jsx";
+import { Flex, Grid, Spacer, styled } from "~/styled-system/jsx";
 import { prisma } from "~/utils/prisma.server";
 
 export const loader = async (args: LoaderFunctionArgs) => {
-  const { params } = args;
+  const { params, request } = args;
   const slug = z.string().parse(params.slug);
+  const url = new URL(request.url);
+  const page = z.coerce.number().parse(url.searchParams.get("page") ?? "1");
 
   const products = await prisma.products.findMany({
     where: {
@@ -15,6 +19,7 @@ export const loader = async (args: LoaderFunctionArgs) => {
       },
     },
     take: 21,
+    skip: (page - 1) * 21,
     orderBy: {
       title: "asc",
     },
@@ -23,24 +28,47 @@ export const loader = async (args: LoaderFunctionArgs) => {
     },
   });
 
-  return products;
+  return { products, page, slug };
 };
 
 const TrackProductsPage = () => {
-  const products = useLoaderData<typeof loader>();
+  const { products, page, slug } = useLoaderData<typeof loader>();
 
   return (
-    <Grid
-      gridTemplateColumns={{ base: "repeat(2, 1fr)", md: "repeat(3, 1fr)" }}
-      gap={2}
-      p={4}
-    >
-      {products.length <= 0 && <styled.p>No products here yet...</styled.p>}
+    <>
+      <Grid
+        gridTemplateColumns={{ base: "repeat(2, 1fr)", md: "repeat(3, 1fr)" }}
+        gap={2}
+        p={4}
+      >
+        {products.length <= 0 && <styled.p>No products here yet...</styled.p>}
 
-      {products.map((product) => (
-        <ProductCard product={product} key={product.slug} />
-      ))}
-    </Grid>
+        {products.map((product) => (
+          <ProductCard product={product} key={product.slug} />
+        ))}
+      </Grid>
+      <Flex p={4} borderTopWidth={1} borderColor="gray.800">
+        {page > 1 && (
+          <LinkButton
+            to={`/tracks/${slug}/products?page=${page - 1}`}
+            variant="secondary"
+          >
+            <RiArrowLeftSLine /> Previous
+          </LinkButton>
+        )}
+
+        <Spacer />
+
+        {products.length >= 21 && (
+          <LinkButton
+            to={`/tracks/${slug}/products?page=${page + 1}`}
+            variant="secondary"
+          >
+            Next <RiArrowRightSLine />
+          </LinkButton>
+        )}
+      </Flex>
+    </>
   );
 };
 
