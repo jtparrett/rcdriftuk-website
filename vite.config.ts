1474600fce7c0cc49ba4dbeb2b8f0ab1363c6f5c
Josh Parrett <jtparrett@me.com>
2026-01-02 21:20:35 +0000

    setup service worker

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,45 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()],
   server: {
     port: 3000,
   },
+  plugins: [
+    reactRouter(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: "autoUpdate",
+      strategies: "generateSW",
+      workbox: {
+        cleanupOutdatedCaches: true,
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => {
+              return request.mode === "navigate";
+            },
+            handler: "StaleWhileRevalidate",
+            options: {
+              cacheName: "pages",
+            },
+          },
+
+          {
+            urlPattern: ({ request }) => {
+              return (
+                request.destination === "" &&
+                request.headers.get("accept")?.includes("application/json")
+              );
+            },
+            handler: "StaleWhileRevalidate",
+            options: {
+              cacheName: "loader-data",
+            },
+          },
+        ],
+      },
+    }),
+  ],
 });
