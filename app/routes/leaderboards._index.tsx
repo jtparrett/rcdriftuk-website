96a9b3fb3af9b26ee7f5babf3ad47e2f64e864d0
Josh Parrett <jtparrett@me.com>
2026-01-06 14:44:18 +0000

    fix redirect

--- app/routes/leaderboards._index.tsx
+++ app/routes/leaderboards._index.tsx
@@ -1,5 +1,5 @@
 import { RiAddCircleFill } from "react-icons/ri";
-import { useLoaderData, type LoaderFunctionArgs } from "react-router";
+import { redirect, useLoaderData, type LoaderFunctionArgs } from "react-router";
 import { LinkButton } from "~/components/Button";
 import { LinkOverlay } from "~/components/LinkOverlay";
 import { TabsBar } from "~/components/TabsBar";
@@ -10,17 +10,21 @@ import { prisma } from "~/utils/prisma.server";
 export const loader = async (args: LoaderFunctionArgs) => {
   const { userId } = await getAuth(args);
 
+  if (!userId) {
+    return redirect("/sign-in?redirect=/leaderboards");
+  }
+
   const leaderboards = await prisma.leaderboards.findMany({
     where: {
       OR: [
         {
-          userId: userId!,
+          userId: userId,
         },
         {
           drivers: {
             some: {
               driver: {
-                id: userId!,
+                id: userId,
               },
             },
           },
